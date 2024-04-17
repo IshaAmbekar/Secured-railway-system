a9ddb341651247eff4122fc860f810aa38cc95e1
Isha Ambekar <isha.ambekar@cumminscollege.in>
2024-04-17 17:14:54 +0530

    added entries

--- api/traindb.py
+++ api/traindb.py
@@ -25,13 +25,29 @@ def initialize_traindatabase():
     # Check if the trains table is empty
     cursor.execute("SELECT COUNT(*) FROM trains")
     row_count = cursor.fetchone()[0]
-    
+
     # If the table is empty, insert sample data into the trains table
     if row_count == 0:
         cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
-                       ('Express Train', '12345', 'City A', 'City B'))
+                       ('Express Train', '12345', 'Mumbai', 'Pune'))
         cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
-                       ('Local Train', '67890', 'City C', 'City D'))
+                       ('Local Train', '67890', 'Delhi', 'Mumbai'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Andhra Pradesh Express', '12723', 'Hyderabad Decan', 'New Delhi'))        
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Ajni Ltt Express', '11202', 'Ajni', 'Lokmanyatilak T'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Chennai Express', '12686', 'Mangalore Cntl', 'Chennai Central'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Coimbatore Express', '16617', 'Rameswaram', 'Coimbatore Junction'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Circar Express', '17644', 'Kakinada Port', 'Chennai Egmore'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Brahmputra Mail', '14056', 'Delhi', 'Dibrugarh'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Bhopal Shatabdi', '12002', 'New Delhi', 'Habibganj'))
+        cursor.execute("INSERT INTO trains (name, number, source, destination) VALUES (%s, %s, %s, %s)",
+                       ('Bangalore Express', '06029', 'Chennai Central', 'Bangalore Cy Junction'))        
         conn.commit()
 
     conn.close()
